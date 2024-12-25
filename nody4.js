5a16f270e210d56302a852ce9c75b9faff7d4541
ArunavaM <arunava6mondal@gmail.com>
2024-12-26 02:04:25 +0530

    Added filtering, sorting & pagination in nody 4

--- nody4.js
+++ nody4.js
@@ -1,8 +1,7 @@
 /*
-  Now we will start configuring mongoose into mongoDB
+  Now we will start with filter, sorting & Pagination
 */
 
-// const { MongoClient, ObjectId } =require('mongodb')
 const express= require('express');
 const mongoose = require('mongoose');
 
@@ -10,16 +9,6 @@ const app = express()
 
 //default middleware to parse request
 app.use(express.json())
-
-
-// mongoDB configuration to connect to db
-// const client = new MongoClient('mongodb://localhost:27017')
-// client.connect().then(()=>{
-//   db=client.db('22_12_db')
-//   console.log('DB connected')
-// }).catch(err =>{
-//   console.error(err)
-// })
 
 //mongoose configuration to connect to db
 mongoose.connect('mongodb://localhost:27017/22_12_db', {
@@ -50,9 +39,62 @@ const User = mongoose.model('User', userSchema)
 
 app.get('/users',async (req,res) => {
   try{
-    // const collection = db.collection('users')
-    // const users = await collection.find().toArray()
-    const users = await User.find()
+    // filter
+    const { age, name} = req.query // suppose we want to filter by age &/or name like /users?age=39 /users?name=John
+    const query = {}
+    if(age) query.age=Number(age)
+    if(name) query.name=new RegExp(name, 'i')
+    /*
+    Why Use RegExp in MongoDB Queries?
+      Partial Matching:
+      If the user searches for ali, the regex new RegExp('ali', 'i') matches:
+      "Alice"
+      "Alina"
+      "Malik"
+      Without a regex, you would need an exact match (name: 'ali'), which is restrictive.
+
+      Case-Insensitive Matching ('i' flag):
+      Matches the string regardless of case.
+      For example:
+      name: 'ALI' matches Ali, aLi, or ali.
+
+      Dynamic Search Criteria:
+      If the search term is passed dynamically (e.g., via req.query.name), regex can adapt to varying input patterns without hardcoding the search logic.
+
+      How It Works in MongoDB ?
+      MongoDB supports regex in queries. For example:
+      db.collection('users').find({ name: /ali/i });
+      This matches all documents where the name field contains the substring ali (case-insensitively).
+    */
+    
+    // sort
+    const {sortBy='name', order='asc'} = req.query
+    const sortOrder = order === 'asc' ? 1 : -1
+
+    //pagination
+    /*
+      for pagination we need 2 functions .skip & .limit 
+      .skip: this can skip mentioned number of documents from first
+      here if we do User.find().skip(20) -> this will skip the first 20 documents and will show the rest
+
+      .limit: this can show limited number of documents
+      so if just called User.find().limit(20) -> this means it will show the first 20 documents
+
+      Hence in pagination we will use power of these two functions,
+      suppose you want to show per page 10 users only ,
+      in 1st page, we will skip 0 documents and will show 10 documents only -> skip(0*10).limit(10) -> skip((Page - 1)*10).limit(10)
+      next you went on 2nd page and want to show next 10 users, then we will skip then first 10 users and will show the next 10 users -> skip(10).limit(10) -> skip((Page-1)*10).limit(10)
+      next you went on 3rd page and want to show next 10 users, then we will skip then first 20 users and will show the next 10 users -> skip(20).limit(10) -> skip((Page-1)*10).limit(10)
+      
+      Hence, we can create a formula, skip((page-1)*limit).limit(limit)
+    */
+    const { page=1, limit=10 } = req.query;
+    const users = await User
+      .find(query)
+      .skip((page-1)* Number(limit))
+      .limit(Number(limit))
+      .sort({[sortBy]: sortOrder})
+      
     res.status(200).json(users)  
   } catch (err) {
     res.status(400).json({error: err.message})
@@ -62,13 +104,7 @@ app.get('/users',async (req,res) => {
 app.post('/users',async (req,res) => {
   console.log(req.body)
   try {
-    // the below is not required because schema will handle this
-    // if(Object.keys(req.body)){
-    //   res.status(200).json({message: 'User details not present'})
-    // }
     const body = req.body
-    // const collection = await db.collection('users')
-    // collection.insertOne(body)
     await new User(body).save() // here .save is required to save the newly created instance from new User(body)
     res.status(200).json({message: 'Suucess'})
   } catch (err){
@@ -78,11 +114,6 @@ app.post('/users',async (req,res) => {
 
 app.put('/users/:id', async (req,res) => {
   try{
-    
-    // const result = await db.collection('users').updateOne(
-    //   {_id: new ObjectId(req.params.id)},
-    //   {$set: req.body}
-    // )
     const user = await User.findByIdAndUpdate(req.params.id,req.body,{
       new: true, // without this mongoose returns the original document before update, so this makes sure updated one is returned
       runValidators: true // without this invalid data can be updated, so this ensures that on every update, the schema validations are run
@@ -100,7 +131,6 @@ app.put('/users/:id', async (req,res) => {
 
 app.delete('/users/:id',async (req,res)=>{
   try{
-    // const result = await db.collection('users').deleteOne({"_id": new ObjectId(req.params.id)})
     const user = await User.findByIdAndDelete(req.params.id)
     if(user){
       res.status(200).json({message: 'User deleted'})
